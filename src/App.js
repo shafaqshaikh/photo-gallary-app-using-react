b09722f858593537539683a3921a39f1f1e6d86a
cab-service-app <geekerscafe@gmail.com>
2021-02-07 15:33:20 +0530

    photo gallary app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React , {useState , useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroll-component'
 function App() {
+
+  const [images , setImages] = useState([])
+ 
+
+  useEffect(() => {
+   fetchImages()
+
+  }, [])
+
+const fetchImages = ()=>{
+  const api="https://api.unsplash.com/"
+  axios.get(`${api}/photos/random?client_id=SMjQTxuSnEFigxlJ0Tz09Ik6TubRDGF6cZKIjm8Qhmc&count=20`)
+  .then(res=> setImages([...images , ...res.data]))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <h1>Photo Gallary App</h1>
+      </center>
+    <InfiniteScroll
+    dataLength={images.length}
+    next ={fetchImages}
+    hasMore={true}
+    loader={<div className="loader"></div>}
+    >
+    {
+       images.map(image=>{
+         return <img key={image.id} src={image.urls.thumb}></img>
+       })
+     }
+    </InfiniteScroll>
+   
     </div>
   );
 }
